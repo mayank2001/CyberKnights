147e46b0da23773feda162b5f8594cc5a8b1a6c5
Mohit Deshpande <mohit.m.deshpande@gmail.com>
2020-09-25 14:59:21 +0530

    updated set budget page and constraint

---
+++ app/src/main/java/com/hackathon/cyberknights/activites/setbudget.java
@@ -0,0 +1,16 @@
+package com.hackathon.cyberknights.activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hackathon.cyberknights.R;
+
+public class setbudget extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_budget);
+    }
+}
