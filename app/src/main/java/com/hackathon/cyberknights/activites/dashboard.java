ae0eab1ac95b3d556496d4a5b10f4f60752b2380
ManasUplekar001 <manasuplekar@gmail.com>
2020-09-25 20:56:22 +0530

    dashboard added

---
+++ app/src/main/java/com/hackathon/cyberknights/activites/dashboard.java
@@ -0,0 +1,16 @@
+package com.hackathon.cyberknights.basic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hackathon.cyberknights.R;
+
+public class dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+    }
+}
