d1aafa45c064a1084d1f09e9dda195b0fd3790da
mayank2001 <55726377+mayank2001@users.noreply.github.com>
2020-09-25 18:18:51 +0530

    config added

--- app/src/main/java/com/hackathon/cyberknights/activites/LoginActivity.java
+++ app/src/main/java/com/hackathon/cyberknights/activites/LoginActivity.java
@@ -1,43 +1,78 @@
 package com.hackathon.cyberknights.activites;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.hackathon.cyberknights.MainActivity;
 import com.hackathon.cyberknights.R;
+
+import javax.xml.datatype.Duration;
+
 public class LoginActivity extends AppCompatActivity {
 
-    EditText txtEmail,txtPassword;
+    EditText txtUsername,txtPassword;
     Button get_otp;
-
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        txtEmail=(EditText)findViewById(R.id.user_name);
-        txtPassword=(EditText)findViewById(R.id.Pass_word);
+        txtUsername = (EditText) findViewById(R.id.user_name);
+        txtPassword = (EditText) findViewById(R.id.Pass_word);
         get_otp = (Button) findViewById(R.id.get_otp);
 
+        firebaseAuth = FirebaseAuth.getInstance();
 
         get_otp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                String username = txtUsername.getText().toString().trim();
+                String password = txtPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(username)) {
+                    Toast.makeText(getApplicationContext(), "Please Enter Email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Please Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
 
 
+                if (password.length() < 6) {
+                    Toast.makeText(getApplicationContext(), "Password is too short", Toast.LENGTH_SHORT).show();
+                }
 
+                firebaseAuth.signInWithEmailAndPassword(username, password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
 
+                                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
 
+                                } else {
 
+                                    Toast.makeText(getApplicationContext(), "Login failed Or User not Registered", Toast.LENGTH_SHORT).show();
+                                }
 
+                            }
+
+                        });
             }
+
         });
-
-
-
-
-    }
-}
+    }}
