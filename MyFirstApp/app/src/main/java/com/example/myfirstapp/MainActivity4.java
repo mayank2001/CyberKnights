a970059f3ac6f57fb37a32d8fa277a42fd30351c
Mohit Deshpande <mohit.m.deshpande@gmail.com>
2020-09-25 09:28:36 +0530

    update
    Add Cash

---
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity4.java
@@ -0,0 +1,15 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+
+    }
+}
