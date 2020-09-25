eebc5846afca69764257db4d07b9a94f051882ed
Anvay Desshmukh <anvayd01@gmail.com>
2020-09-25 21:13:22 +0530

    setbudget

--- app/src/main/java/com/hackathon/cyberknights/activites/setbudget.java
+++ app/src/main/java/com/hackathon/cyberknights/activites/setbudget.java
@@ -2,15 +2,34 @@ package com.hackathon.cyberknights.activites;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.hackathon.cyberknights.R;
 
 public class setbudget extends AppCompatActivity {
 
+    EditText editText;
+
+    public void  setBudget(View view) {
+         Intent intent = new Intent(setbudget.this, com.hackathon.cyberknights.basic.dashboard.class );
+
+         startActivity(intent);
+
+
+
+
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_budget);
+
+
+        editText = (EditText) findViewById(R.id.setBudget);
     }
 }
