68ec1c89358c051640f13e353ffdf8ec7415fe53
mayank2001 <55726377+mayank2001@users.noreply.github.com>
2020-09-26 10:48:29 +0530

    piechart

--- app/src/main/java/com/hackathon/cyberknights/activites/dashboard.java
+++ app/src/main/java/com/hackathon/cyberknights/activites/dashboard.java
@@ -4,11 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,21 +23,31 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.hackathon.cyberknights.R;
 
+import org.w3c.dom.Text;
+
 import java.sql.DatabaseMetaData;
 import java.util.ArrayList;
 import java.util.Objects;
 
 public class dashboard extends AppCompatActivity {
-    Button logout;
+    Button logout, addAmt;
     FirebaseAuth firebaseAuth;
     DatabaseReference databaseReference;
     FirebaseDatabase firebaseDatabase;
     DataSnapshot dataSnapshot;
     String TAG = "DashboardActivity";
-public void report (View view){
-   Intent intent = new Intent(dashboard.this, piechart.class);
-    startActivity(intent);
-};
+
+    public void report(View view) {
+        Intent intent = new Intent(dashboard.this, piechart.class);
+        startActivity(intent);
+    }
+
+    ;
+
+
+    TextView totalBalance, FullName, userEmail;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,16 +55,15 @@ public void report (View view){
 
         firebaseAuth = FirebaseAuth.getInstance();
 
-        final TextView totalBalance = findViewById(R.id.balanceGet);
+        totalBalance = findViewById(R.id.balanceGet);
 
-        final TextView FullName = findViewById(R.id.Full_Name);
+        FullName = findViewById(R.id.Full_Name);
 
-        final TextView userEmail = findViewById(R.id.userEmail);
-        databaseReference  = FirebaseDatabase.getInstance().getReference();
-
-
+        userEmail = findViewById(R.id.userEmail);
+        databaseReference = FirebaseDatabase.getInstance().getReference();
 
 
+        addAmt = findViewById(R.id.addAmt);
 
 
         databaseReference.addValueEventListener(new ValueEventListener() {
@@ -62,16 +73,11 @@ public void report (View view){
 
                 //getters
                 String totalBaclans = Objects.requireNonNull(snapshot.child(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid()).child("UserProfile").child("monthlyIncome").getValue()).toString();
-                 totalBalance.setText(totalBaclans);
+                totalBalance.setText(totalBaclans);
 
-                 String userName = Objects.requireNonNull(snapshot.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("UserProfile").child("username").getValue()).toString();
+                String userName = Objects.requireNonNull(snapshot.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("UserProfile").child("username").getValue()).toString();
                 FullName.setText(userName);
                 userEmail.setText(FirebaseAuth.getInstance().getCurrentUser().getEmail());
-
-
-
-
-
 
 
             }
@@ -83,7 +89,7 @@ public void report (View view){
         });
 
 
-        logout.setOnClickListener(new View.OnClickListener() {
+        /*logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FirebaseAuth.getInstance().signOut();
@@ -91,27 +97,37 @@ public void report (View view){
                 startActivity(new Intent(dashboard.this,LoginActivity.class));
                 finish();
             }
+        });*/
+
+        final ListView myListView = findViewById(R.id.transactions);
+        final ArrayList<String> transactionlist = new ArrayList<String>();
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, transactionlist);
+
+
+        final EditText catego = findViewById(R.id.categoryTV);
+        final EditText amtTV = findViewById(R.id.amtTv);
+        addAmt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                transactionlist.add(catego.getText().toString() + " = " + amtTV.getText().toString());
+                int Total = Integer.parseInt(totalBalance.getText().toString());
+                int Expense = Total - Integer.parseInt(amtTV.getText().toString());
+                Log.d(TAG, "onClick: 0"+Expense);
+
+                myListView.setAdapter(arrayAdapter);
+            }
         });
 
-        ListView myListView = findViewById(R.id.transactions);
-        ArrayList<String> transactionlist = new ArrayList<String>();
-        transactionlist.add("Shopping = 5000Rs");
-        transactionlist.add("Miscellaneous = 2000Rs");
-        transactionlist.add("Food = 1500Rs");
-        transactionlist.add("Others = 500Rs");
-
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,transactionlist);
-        myListView.setAdapter(arrayAdapter);
 
     }
 
-    @Override
     protected void onStart() {
         super.onStart();
 
 
     }
-
 
 
 }
--- app/src/main/java/com/hackathon/cyberknights/activites/piechart.java
+++ app/src/main/java/com/hackathon/cyberknights/activites/piechart.java
@@ -4,49 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.anychart.AnyChart;
-import com.anychart.AnyChartView;
-import com.anychart.chart.common.dataentry.DataEntry;
-import com.anychart.chart.common.dataentry.ValueDataEntry;
-import com.anychart.charts.Pie;
 import com.hackathon.cyberknights.R;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class piechart extends AppCompatActivity {
-
-
-
-    AnyChartView anyChartView;
-
-    String[] transactions = {"Shopping", "Food", "Miscellaneous", "Others"};
-    int [] money = {5000, 1500, 2000, 500};
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.piechart);
 
-        anyChartView = findViewById(R.id.any_char_view);
-
-        setupPieChart();
-
     }
 
-    public void setupPieChart()  {
 
-        Pie pie = AnyChart.pie();
-        List<DataEntry> dataEntries = new ArrayList<>();
-        for (int i = 0; i < transactions.length; i++){
-            dataEntries.add(new ValueDataEntry(transactions[i], money[i]));
-            
-        }
-
-        pie.data(dataEntries);
-        anyChartView.setChart(pie);
-
-
-    }
 }
